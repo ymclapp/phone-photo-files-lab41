75d818dbe1b8b8105126b8740e999c10fbe5f628
ymcla <ymclapp@yahoo.com>
2021-12-07 19:29:11 -0600

    tried to get navigation to work.  Breaks app.

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { AppearanceProvider } from 'react-native-appearance';
@@ -7,6 +9,28 @@ import * as Sharing from 'expo-sharing';
 import logo from './assets/logo.png';
 import { ThemeProvider } from './theme/ThemeProvider';
 import { Screen, Switch, Message } from './components';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   const [selectedImage, setSelectedImage] = React.useState(null);
@@ -55,22 +79,29 @@ export default function App() {
   return (
     <AppearanceProvider>
       <ThemeProvider>
-      <Screen>
-        <View style={styles.container}>
-          <Image source={logo} style={{ width: 305, height: 159 }} />
-          <Text style={styles.instructions}>
-            Click the button below to save the image to a file location
-          </Text>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Details" component={DetailsScreen} />
+            <Screen>
+              <View style={styles.container}>
+                <Image source={logo} style={{ width: 305, height: 159 }} />
+                <Text style={styles.instructions}>
+                  Click the button below to save the image to a file location
+                </Text>
 
-          <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
-            <Text style={styles.buttonText} > Pick a Photo</Text>
-          </TouchableOpacity>
-          <StatusBar style="auto" />
-        </View>
+                <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+                  <Text style={styles.buttonText} > Pick a Photo</Text>
+                </TouchableOpacity>
+                <StatusBar style="auto" />
+              </View>
 
-          <Switch />
-          <Message />
-        </Screen>
+              <Switch />
+              <Message />
+            </Screen>
+
+          </Stack.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
     </AppearanceProvider>
   );
